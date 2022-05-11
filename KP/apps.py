18a13ac7558709c191a7715d05e767eddc259398
Yogesh Mangal <67264564+yogeshmangal@users.noreply.github.com>
2022-05-11 09:23:45 +0530

    New Project

---
+++ KP/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KpConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'KP'
---
+++ KP/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django import views
+from . import views
+from django.shortcuts import render
+from django.conf.urls.static import static
+from django.conf import settings
+
+admin.site.site_header = "Knowledge Platform Admin"
+admin.site.site_title = "Knowledge Platform Admin Portal"
+admin.site.index_title = "Welcome to CData Knowledge Platform"
+
+urlpatterns = [
+    path('',views.home, name='home'),
+]
