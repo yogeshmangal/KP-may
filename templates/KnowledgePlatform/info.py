945d4cad59941e27e68089b50851b7014205b01c
Shashank Shukla <44723604+shash137@users.noreply.github.com>
2022-05-12 09:51:59 +0530

    Initial commit

---
+++ templates/KnowledgePlatform/info.py
@@ -0,0 +1,6 @@
+#it will contain information about the email account 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'knowledgeplatform64@gmail.com'
+EMAIL_HOST_PASSWORD = 'knowledge@123'
+EMAIL_PORT = 587
